e1c1fbe7dc2a3b7db4ace11b15c30f4da79b4d02
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-17 19:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string===string.toLowerCase()) {
-    return "I can\'t hear you!"
+    return "I can\'t hear you!";
+    else if (string===string.toUpperCase())
+      
   }
 }
+
+
 
 
 
